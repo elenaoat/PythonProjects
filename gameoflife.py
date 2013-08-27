33bcba0d9e7af21b730740e6406cc23b6d38f1c0
Elena Oat <oat.elena@gmail.com>
2013-08-27 16:42:54 +0300

    add small changes with list comprehensions

--- gameoflife.py
+++ gameoflife.py
@@ -4,39 +4,44 @@ import sys
 width = 640
 height = 400
 
-i=0
+i = 0
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 dots = []
-dot_num = random.randint(5, 50)
-for x in range(dot_num):
-    x = random.randint(1, 10)
-    y = random.randint(1, 10)
-    dots.append((x*10, y*10))
+#dot_num = random.randint(5, 50)
+#for x in range(dot_num):
+#    x = random.randint(1, 10)
+#    y = random.randint(1, 10)
+#    dots.append((x*10, y*10))
 
-#dots.append((100, 100))
-#dots.append((110, 100))
-#dots.append((120, 100))
-#dots.append((110, 90))
-#dots.append((100, 90))
-#dots.append((90, 90))
+dots.append((100, 100))
+dots.append((110, 100))
+dots.append((120, 100))
+dots.append((110, 90))
+dots.append((100, 90))
+dots.append((90, 90))
 run = True
 def draw_dot(coords, red, green, blue):
     screen.set_at(coords, (red, green, blue))
 
+def populate_list(k, dot):
+    k.append((dot[0] + 10, dot[1]))
+    k.append((dot[0] - 10, dot[1]))
+    k.append((dot[0], dot[1] + 10))
+    k.append((dot[0], dot[1] - 10))
+    k.append((dot[0] + 10, dot[1] + 10))
+    k.append((dot[0] - 10, dot[1] - 10))
+    k.append((dot[0] - 10, dot[1] + 10))
+    k.append((dot[0] + 10, dot[1] - 10))
+
+
 def find_new():
     dots_new = set()
+
     for dot in dots: 
         k = []
-        k.append((dot[0] + 10, dot[1]))
-        k.append((dot[0] - 10, dot[1]))
-        k.append((dot[0], dot[1] + 10))
-        k.append((dot[0], dot[1] - 10))
-        k.append((dot[0] + 10, dot[1] + 10))
-        k.append((dot[0] - 10, dot[1] - 10))
-        k.append((dot[0] - 10, dot[1] + 10))
-        k.append((dot[0] + 10, dot[1] - 10))
+        populate_list(k, dot)
         for elem in k:
             if elem not in dots:
                 n = calculate_neighbours(elem)
@@ -47,14 +52,8 @@ def find_new():
 def calculate_neighbours(dot):
     neighbours = 0
     k = []
-    k.append((dot[0]-10, dot[1]))
-    k.append((dot[0]+10, dot[1]))
-    k.append((dot[0], dot[1]-10))
-    k.append((dot[0], dot[1]+10))
-    k.append((dot[0]-10, dot[1]-10))
-    k.append((dot[0]+10, dot[1]+10))
-    k.append((dot[0]-10, dot[1]+10) )
-    k.append((dot[0]+10, dot[1]-10))
+    populate_list(k, dot)
+   
     for elem in k:
         if elem in dots:
             neighbours += 1
@@ -70,21 +69,15 @@ while run:
 #        pygame.quit()
 #        sys.exit()
     for dot in dots:
-        print dot
         draw_dot(dot, red, green, blue)
     pygame.display.flip()
 
-    dots_to_remove = []
     dots_new = find_new()
-    for dot in dots:        
-        neigh = calculate_neighbours(dot)
-        if neigh not in [2, 3]:
-            dots_to_remove.append(dot)
-            draw_dot(dot, 0, 0, 0)
+    dots_to_remove = [dot for dot in dots if calculate_neighbours(dot) not in [2, 3]]
+    [draw_dot(dot, 0, 0, 0) for dot in dots_to_remove]
     dots = [dot for dot in dots if dot not in dots_to_remove] 
-    
-    for dot in dots_new:
-       dots.append(dot)
+    dots = dots + list(dots_new)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
